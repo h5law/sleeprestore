f546d5bf726738d8bbfd213d49db24c4297bd153
Harry Law <53987565+h5law@users.noreply.github.com>
2022-07-06 09:18:44 +0100

    Initial commit

---
+++ src/main/java/com/github/h5law/sleeprestore/SleepRestore.java
@@ -0,0 +1,17 @@
+package com.github.h5law.sleeprestore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SleepRestore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
